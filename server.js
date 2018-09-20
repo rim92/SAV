61b900f3edf63240cf175fb25a98f7c08b8d1299
rim92 <rim.methni@esprit.tn>
2018-09-20 11:10:17 +0100

    passport

--- server.js
+++ server.js
@@ -91,20 +91,54 @@ app.use(function(req, res, next) {
 var db;
 // Connect to the database before starting the application server.
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://rimi:RIMA23379665@ds125422.mlab.com:25422/sav",{ useMongoClient: true }, function (err, client) {
+//mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://rimi:RIMA23379665@ds125422.mlab.com:25422/sav",{ useMongoClient: true }, function (err, client) {
+//  if (err) {
+//    console.log(err);
+//    process.exit(1);
+//  }
+//
+//  // Save database object from the callback for reuse.
+//  db = client.db();
+//  console.log("Database connection ready");
+//
+//  // Initialize the app.
+//  var server = app.listen(process.env.PORT || 8080, function () {
+//    var port = server.address().port;
+//    console.log("App now running on port", port);
+//  });
+//});
+
+
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://rimi:RIMA23379665@ds125422.mlab.com:25422/sav';      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
   if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+    // do some work here with the database.
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+    //Close connection
+    db.close();
+	  
+	  
+	  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+	  
+  }
 });
-
